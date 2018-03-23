660c5714d8ff5110e69ed67e6d5cc518d46db3e2
Roland N Green <green349@min.ecn.purdue.edu>
2018-03-23 14:37:37 -0400

    Add mycfs forward declarations and set priority

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1028,6 +1028,7 @@ struct sched_class {
 extern const struct sched_class stop_sched_class;
 extern const struct sched_class rt_sched_class;
 extern const struct sched_class fair_sched_class;
+extern const struct sched_class mycfs_sched_class;
 extern const struct sched_class idle_sched_class;
 
 
